0d54e9adb3329e9ac552002e6691d742ee4b0dc2
Tiago Alves <tiagooliveiraaso2@gmail.com>
2023-12-09 12:16:05 -0300

    vuetify baixado

---
+++ resources/js/todo-list/src/js/vuetify.js
@@ -0,0 +1,13 @@
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import * as labsComponents from 'vuetify/labs/components'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  labsComponents
+});
+
+export default vuetify;
