118449422c52fc55514e6864cdb136ea9e73e6ad
Tiago Alves <tiagooliveiraaso2@gmail.com>
2023-12-09 18:53:30 -0300

    metodos da task

--- app/Http/Controllers/API/TaskController.php
+++ app/Http/Controllers/API/TaskController.php
@@ -2,16 +2,28 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\Controller;
+use App\Models\Task;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class TaskController extends Controller
 {
     public function index()
     {
     }
-    public function create()
+    public function create(Request $request)
     {
+        try {
+            Task::create([
+                'title' => $request->title,
+            ]);
+            return [
+                'status' => 'success',
+                'cacheble'=>false,
+            ];
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
     }
     public function update()
     {
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+    protected $fillable = ['user_id','title','conclued'];
+    //cast conclued
+
+    //relacionamento
 }
