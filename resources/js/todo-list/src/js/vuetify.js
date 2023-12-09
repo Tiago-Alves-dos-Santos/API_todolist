c65aab7ad85fb448d0505c39a52297b794290e6a
Tiago Alves <tiagooliveiraaso2@gmail.com>
2023-12-09 12:22:11 -0300

    icones mdi baixados

--- resources/js/todo-list/src/js/vuetify.js
+++ resources/js/todo-list/src/js/vuetify.js
@@ -1,13 +1,17 @@
-import 'vuetify/styles'
+import 'vuetify/styles';
+import '@mdi/font/css/materialdesignicons.css';
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import * as labsComponents from 'vuetify/labs/components'
 
 const vuetify = createVuetify({
-  components,
-  directives,
-  labsComponents
+    components,
+    directives,
+    labsComponents,
+    icons: {
+        defaultSet: 'mdi', // This is already the default value - only for display purposes
+    },
 });
 
 export default vuetify;
