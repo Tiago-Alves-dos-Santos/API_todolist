2eb155ae73d4ff9cef321b1dd170d80eeb32f88f
Tiago Alves <tiagooliveiraaso2@gmail.com>
2023-12-09 16:38:51 -0300

    controller e rota de login

---
+++ app/Http/Controllers/API/TaskController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+    }
+    public function create()
+    {
+    }
+    public function update()
+    {
+    }
+    public function delete()
+    {
+    }
+    private function search()
+    {
+    }
+}
