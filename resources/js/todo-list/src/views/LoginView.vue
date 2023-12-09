3bff86e36e205f8cdb6ea57cf5ab3936fa46e369
Tiago Alves <tiagooliveiraaso2@gmail.com>
2023-12-09 17:42:28 -0300

    login token

--- resources/js/todo-list/src/views/LoginView.vue
+++ resources/js/todo-list/src/views/LoginView.vue
@@ -2,24 +2,26 @@
     <div class="home">
         <center-layout>
             <v-card title="Login" class="mx-auto" width="500" style="padding: 10px;">
-                <v-text-field label="Email" hide-details="auto" variant="outlined"></v-text-field>
-                <password-input variant="outlined" class="mt-1"></password-input>
-                <div class="d-flex justify-end mt-2">
-                    <v-btn color="primary" class="mr-2">
-                        Login
-                    </v-btn>
-                </div>
+                <form @submit.prevent='login'>
+                    <v-text-field label="Email" hide-details="auto" variant="outlined" v-model="email"></v-text-field>
+                    <password-input variant="outlined" class="mt-1" v-model="password"></password-input>
+                    <div class="d-flex justify-end mt-2">
+                        <v-btn type="submit" color="primary" class="mr-2">
+                            Login
+                        </v-btn>
+                    </div>
+                </form>
             </v-card>
         </center-layout>
     </div>
 </template>
 
 <script>
-// @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
 import CenterLayout from '@/layouts/CenterLayout.vue';
 import PasswordInput from '@/components/Input/PasswordInput.vue';
 import axios from 'axios';
+import API from '@/js/api';
+import router from '@/router/index';
 export default {
     name: 'HomeView',
     components: {
@@ -27,15 +29,35 @@ export default {
         PasswordInput
 
     },
-    mounted() {
-        axios.get('https://viacep.com.br/ws/01001000/json/')
-            .then(function (response) {
-                console.log(response.data);
-                console.log(response.status);
-                console.log(response.statusText);
-                console.log(response.headers);
-                console.log(response.config);
+    data: () => {
+        return {
+            email: '',
+            password: ''
+        }
+    },
+    methods: {
+        login() {
+            axios.get(API.auth.operations.login.url, {
+                params: {
+                    email: this.email,
+                    password:this.password
+                }
+            }
+            ).then(function (response) {
+                let data = response.data;
+                if(response.status == '200' && data.cacheble) {
+                    localStorage.setItem('token', data.token);
+                    router.push({ name: 'home' });
+                }
+            }).catch((errors) => {
+                console.log(errors);
+                console.log(errors.response.data.errors);
+                // console.log(response.data.errors);
             });
+        }
+    },
+    mounted() {
+
 
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,8 +18,4 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/login', function (Request $request) {
-    return [
-        'login' => 'aq'
-    ];
-});
+Route::get('/login', [LoginController::class,'login']);
